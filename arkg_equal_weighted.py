ff882bbd16b491e11bd8d6fef2406cdb1cc9ac1e
Neelay Das <61340618+dasneelay@users.noreply.github.com>
2024-06-10 13:06:08 -0700

    Removed API token

--- arkg_equal_weighted.py
+++ arkg_equal_weighted.py
@@ -9,7 +9,7 @@ holding to purchase to get an equal-weight version of the fund.
 
 '''
 
-IEX_CLOUD_API_TOKEN = 'Tpk_059b97af715d417d9f49f50b51b1c448'
+IEX_CLOUD_API_TOKEN = ''
 
 # Read fund holdings
 stocks = pd.read_csv('~/Documents/projects/ARKproject/arkg_holdings.csv')
